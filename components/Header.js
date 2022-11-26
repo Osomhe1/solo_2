f0534316c03b55fb0a87c17451886020e0c055f7
Osomhe1 <osomhe.aleogho@gmail.com>
2022-11-26 23:47:09 +0100

    update

--- components/Header.js
+++ components/Header.js
@@ -55,18 +55,20 @@ export default function Header() {
           }
         >
           <nav className='flex-col flex-grow '>
-            <ul className='flex flex-grow justify-end px-2 flex-wrap items-center'>
+            <ul className='grid  md:flex md:flex-grow justify-end px-12 md:px-2 py-3 md:py-0 md:flex-wrap  items-center'>
               <li>
                 <Link
                   href='/howitworks'
-                  className='font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out'
+                  className='font-medium text-gray-600 hover:text-gray-900 px-5 py-1 flex items-center
+                   transition duration-150 ease-in-out'
                 >
                   How It Works
                 </Link>
               </li>
               <li>
                 <Link
-                  className='font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out'
+                  className='font-medium text-gray-600 hover:text-gray-900 px-5 py-1 flex items-center
+                   transition duration-150 ease-in-out'
                   href='/price/Price'
                 >
                   Pricing
@@ -74,15 +76,17 @@ export default function Header() {
               </li>
               <li>
                 <Link
-                  className='font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out'
+                  className='font-medium text-gray-600 hover:text-gray-900 px-5 py-1 flex items-center transition duration-150 ease-in-out'
                   href='/portals'
                 >
                   NFC Portals
                 </Link>
               </li>
-              <li>
+              <li className='py-1 md:py-0'>
                 <Link
-                  className='inline-flex items-center px-4 py-2 mt-2 font-medium text-blue-600 transition border-2 border-blue-600 duration-500 ease-in-out transform bg-transparent rounded-lg text-md md:mt-0 md:ml-4 bg-white mx-4'
+                  className='inline-flex items-center px-4 py-2 mt-2 font-medium text-blue-600 transition
+                   border-2 border-blue-600 duration-500 ease-in-out transform bg-transparent rounded-lg 
+                   text-md md:mt-0 md:ml-4 bg-white mx-4'
                   href='/auth/Login'
                 >
                   <span className='justify-center'>Sign In</span>
@@ -93,9 +97,11 @@ export default function Header() {
                   ></svg>
                 </Link>
               </li>
-              <li>
+              <li className='py-1 md:py-0'>
                 <Link
-                  className='inline-flex items-center px-4 py-2 mt-2 font-medium text-blue-600 transition border-2 border-blue-600 duration-500 ease-in-out transform bg-transparent rounded-lg text-md md:mt-0 md:ml-4 bg-white mx-4'
+                  className='inline-flex items-center px-4 py-2 mt-2  font-medium text-blue-600 transition
+                   border-2 border-blue-600 duration-500 ease-in-out transform bg-transparent rounded-lg
+                    text-md md:mt-0 md:ml-4 bg-white mx-4'
                   href='/auth/SignUp'
                 >
                   <span className='justify-center'>Create Account</span>
