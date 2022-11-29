9a4f8a6ef18cb871de71b8abaede12d1a911ee16
Osomhe1 <osomhe.aleogho@gmail.com>
2022-11-29 12:27:07 +0100

    dashboard update

--- components/Appreance.js
+++ components/Appreance.js
@@ -1,92 +1,13 @@
 // import { Fragment } from 'react'
 // import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid'
-// import { faHatChef } from '@fortawesome/sharp-solid-svg-icons'
 
-// function classNames(...classes) {
-//   return classes.filter(Boolean).join(' ')
-// }
+
 
 export default function Appreance() {
   return (
     <div className=''>
-      {/* <Menu as='div' className='relative inline-block text-left w-full'>
-      <div>
-        <Menu.Button className='inline-flex w-full justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100'>
-          Appreance
-          <ChevronDownIcon className='-mr-1 ml-2 h-5 w-5' aria-hidden='true' />
-        </Menu.Button>
-      </div>
-
-      <Transition
-        as={Fragment}
-        enter='transition ease-out duration-100'
-        enterFrom='transform opacity-0 scale-95'
-        enterTo='transform opacity-100 scale-100'
-        leave='transition ease-in duration-75'
-        leaveFrom='transform opacity-100 scale-100'
-        leaveTo='transform opacity-0 scale-95'
-      >
-        <Menu.Items className='absolute right-0 z- mt- w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-          <div className='py-1'>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href='#'
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Account settings
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href='#'
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Support
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href='#'
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  License
-                </a>
-              )}
-            </Menu.Item>
-            <form method='POST' action='#'>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    type='submit'
-                    className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block w-full px-4 py-2 text-left text-sm'
-                    )}
-                  >
-                    Sign out
-                  </button>
-                )}
-              </Menu.Item>
-            </form>
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu> */}
+      
 
       {/* <!-- Start block --> */}
 
@@ -101,19 +22,7 @@ export default function Appreance() {
                  items-center '
                 >
                   Appearnces
-                  {/* <svg
-                    data-accordion-icon=''
-                    class='w-6 h-6  shrink-0'
-                    fill='currentColor'
-                    viewBox='0 0 20 20'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      fill-rule='evenodd'
-                      d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
-                      clip-rule='evenodd'
-                    ></path>
-                  </svg> */}
+                 
                   <ChevronDownIcon className='text- h-14' />
                 </summary>
                 <div className='px-4 pb-4'>
@@ -204,6 +113,17 @@ export default function Appreance() {
                       Round
                     </button>
                   </div>
+                </div>
+                <div className='text-center mt-6 float-right '>
+                  <button
+                    className='bg-blue-500 text-white active:bg-gray-700 text-sm font-bold uppercase
+                       px-6 py-5 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-[100px]
+                       bottom-0 '
+                    type='button'
+                    style={{ transition: 'all .15s ease' }}
+                  >
+                    save
+                  </button>
                 </div>
               </details>
             </div>
--- components/profile/Profile.js
+++ components/profile/Profile.js
@@ -87,7 +87,7 @@ export default function Profile() {
             style={{ height: '200px', backgroundColor: 'white' }}
           >
             <div
-              className='absolute top-0 w-full h-full bg-center bg-cover bg-blue-500'
+              className='absolute top-0 w-full h-full bg-center bg-cover bg-blue-600'
               // style={{
               //   backgroundImage:
               //     "url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2710&q=80')",
@@ -140,39 +140,8 @@ export default function Profile() {
                         />
                       </div>
                     </div>
-                    {/* <div className='w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center'>
-                    <div className='py-6 px-3 mt-32 sm:mt-0'>
-                      <button
-                        className='bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1'
-                        type='button'
-                        style={{ transition: 'all .15s ease' }}
-                      >
-                        Connect
-                      </button>
-                    </div>
-                  </div> */}
-                    {/* <div className='w-full lg:w-4/12 px-4 lg:order-1'>
-                    <div className='flex justify-center py-4 lg:pt-4 pt-8'>
-                      <div className='mr-4 p-3 text-center'>
-                        <span className='text-xl font-bold block uppercase tracking-wide text-gray-700'>
-                          22
-                        </span>
-                        <span className='text-sm text-gray-500'>Friends</span>
-                      </div>
-                      <div className='mr-4 p-3 text-center'>
-                        <span className='text-xl font-bold block uppercase tracking-wide text-gray-700'>
-                          10
-                        </span>
-                        <span className='text-sm text-gray-500'>Photos</span>
-                      </div>
-                      <div className='lg:mr-4 p-3 text-center'>
-                        <span className='text-xl font-bold block uppercase tracking-wide text-gray-700'>
-                          89
-                        </span>
-                        <span className='text-sm text-gray-500'>Comments</span>
-                      </div>
-                    </div>
-                  </div> */}
+                    
+                   
                   </div>
 
                   <div className='text- mt-12  '>
